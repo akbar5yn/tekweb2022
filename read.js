4a11fe0c8e40d249fcdb201078b5035f3e15bf64
akbar5yn <akbar2000016129@webmail.uad.ac.id>
2022-05-10 21:36:09 +0700

    update

--- read.js
+++ read.js
@@ -1,33 +1,30 @@
 Vue.createApp({
-    data() {
-      return {       
-        article: null,
-      };
+  data() {
+    return {
+      article: null,
+    };
+  },
+  methods: {
+    getMarkdownData() {
+      const queryString = window.location.search;
+      const urlParams = new URLSearchParams(queryString);
+      const article = urlParams.get('article');
+      var converter = new showdown.Converter();
+      console.log(article);
+      axios
+        .get('https://raw.githubusercontent.com/akbar5yn/tekweb2022/finalMarkdown/contents/' + article)
+
+        .then((res) => {
+          var html = converter.makeHtml(res.data);
+          this.article = html;
+          console.log(html);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
-    methods: {
-      getMarkdownData()
-      {
-        const queryString = window.location.search;
-        const urlParams = new URLSearchParams(queryString);
-        const article = urlParams.get('article');        
-        var converter = new showdown.Converter();
-        console.log(article);
-        axios
-          .get(
-            "https://raw.githubusercontent.com/akbar5yn/tekweb2022/finalMarkdown/contents/"+article
-          )
-          .then((res) => {
-            var html = converter.makeHtml(res.data);           
-            this.article = html;
-            console.log(html);
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      }
-    },
-    beforeMount() {
-      this.getMarkdownData();
-    },
-  }).mount("#app");
-  
+  },
+  beforeMount() {
+    this.getMarkdownData();
+  },
+}).mount('#app');
