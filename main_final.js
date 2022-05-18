0f7023beb887fdd59e10ccc0fc73a933d0d632cc
akbar5yn <akbar2000016129@webmail.uad.ac.id>
2022-05-19 05:46:30 +0700

    menambahkan animasi js

--- main_final.js
+++ main_final.js
@@ -1,83 +1,4 @@
 Vue.createApp({
-    data() {
-      return {
-        message: "Hello Vue!",
-        jumbotron: {},
-        about: {},
-        articles: [],
-        project: [],
-        judul: {}
-      };
-    },
-    methods: {
-      getJumbotron() {
-        axios
-          .get(
-            "https://raw.githubusercontent.com/akbar5yn/tekweb2022/master/contents/jumbotron.json"
-          )
-          .then((res) => {
-            this.jumbotron = res.data;
-            this.getArticles()
-            this.getAbout()
-            this.getPorject()
-            this.getJudul()
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      },
-      getAbout() {
-        axios
-          .get(
-            "https://raw.githubusercontent.com/akbar5yn/tekweb2022/master/contents/about.json"
-          )
-          .then((res) => {
-            this.about = res.data;
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      },
-      getArticles() {
-        axios
-          .get(
-            "https://raw.githubusercontent.com/akbar5yn/tekweb2022/finalMarkdown/contents/articles.json"
-          )
-          .then((res) => {
-            this.articles = res.data;
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      },
-      getPorject() {
-        axios
-          .get(
-            "https://raw.githubusercontent.com/akbar5yn/tekweb2022/master/contents/project.json"
-          )
-          .then((res) => {
-            this.project = res.data;
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      },
-      getJudul() {
-        axios
-          .get(
-            "https://raw.githubusercontent.com/akbar5yn/tekweb2022/master/contents/judul.json"
-          )
-          .then((res) => {
-            this.judul = res.data;
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      },
-    },
-    beforeMount() {
-      this.getJumbotron()
-    },Vue.createApp({
   data() {
     return {
       message: 'Hello Vue!',
@@ -207,5 +128,3 @@ window.onload = function () {
   css.innerHTML = '.txt-rotate > .wrap { border-right: 0.08em solid #666 }';
   document.body.appendChild(css);
 };
-
-  }).mount("#app");
